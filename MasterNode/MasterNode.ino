69b07734ea0d94fbd3615400f5ef1a21923ff1fd
vipin kumar singhal <singhalvipin5697@gmail.com>
2020-04-20 11:01:31 +0530

    Update MasterNode.ino
    Functionalities : 
    * Now network is bidirectional (Topology change- master node is sending packet which can be packet request or command packet 
       to one slave node at a time and in reply that slave is sending it's telemetry packet)
    * msgID synchronization
    * time sync between all nodes of network
    * Route msg from server to slave if it has new msgID (check)
    Flow : 
    *  Master node is  requesting data  from MySQL command table and check if is it new packet (With new msgID). If no then ignores.
    *  If yes then sends that packet to slave node. Node Id is mentioned in packet . If no then master creates it's own request packet and 
       send it to slave
    *  In reply master getting telemetry packet from slave and master will send it to MySQL database.
    *  typedef struct struct_message { 
      int nodeID;
      int RXnodeID;
      byte msgType;
      int msgID;
      byte iCommType;
      unsigned long timeStamp;
      float tempAir;
      float latitudeGPS;
      float longitudeGPS;
      int ledStatus;
    } struct_message;

--- MasterNode/MasterNode.ino
+++ MasterNode/MasterNode.ino
@@ -8,94 +8,144 @@
   this project also realess in GitHub:
   https://github.com/Heltec-Aaron-Lee/WiFi_Kit_series
 */
-
-#include "heltec.h"
-
+#include <ArduinoJson.h>
+#define node 10
+int slaveNode[] = {8,9};
+///////////////////////////////////////////////////////////
 #include "esp_http_client.h"
 #include <WiFi.h>
 #include "Arduino.h"
 
 const char* ssid = "Aliyance";
 const char* password = "mech@1234";
+const char *post_url = "http://192.168.0.103:5000/api/insertIntoTbl"; // Location to send POSTed data
+const char *get_url = "http://192.168.0.103:5000/api/getDataFromOnOffTbl";
 
-const char *post_url = "http://192.168.0.107:5000/api/insertIntoTbl"; // Location to send POSTed data
-//const char *post_data = "id='5555'&node_id='655'&comm_type='wifi'&timestamp='8855'&temperature='67'&gps_coord='122.445566,33.442211'"; 
-
-bool internet_connected = false;
-
-byte localAddress = 0xAA;     // address of this device
-byte destination = 0xBB;      // destination to send to
-
-bool receiveflag = false; // software flag for LoRa receiver, received data makes it true.
-
+#include <HTTPClient.h>
+///////////////////////////////////////////////////////////
+#include "heltec.h"
 #define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
+
+int msgCount = 1;
+long lastSendTime = 0;        // last send time
+int interval = 1200;          // interval between sends
+
+byte COMTYPE_LoRa = 0x01;
+byte COMTYPE_WiFi = 0x02;
+byte COMTYPE_LTE = 0x03;
+byte COMTYPE_ZigBee = 0x04;
+
+byte msgType_CheckStatus = 0x01;
+byte msgType_Command = 0x02;
+byte msgType_Telemtry = 0x03;
 
 typedef struct struct_message {     //Structure example to send telemtryPacket    //Must match the receiver structure
   int nodeID;
+  int RXnodeID;
+  byte msgType;
   int msgID;
   byte iCommType;
   unsigned long timeStamp;
   float tempAir;
   float latitudeGPS;
   float longitudeGPS;
+  int ledStatus;
 } struct_message;
 
-struct_message telemtryPacket;        // Create a struct_message
+struct_message telemtryPacketRX;        // Create a struct_message
+struct_message telemtryPacketTX;        // Create a struct_message
 
 void setup() {
-  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
-  LoRa.onReceive(onReceive);      //WIFI Kit series V1 not support Vext control
   Serial.begin(115200);
-  LoRa.receive();
-
+  
+  Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
+  LoRa.setTxPower(14,RF_PACONFIG_PASELECT_PABOOST);
+  
     if (init_wifi()) { // Connected to WiFi
-    internet_connected = true;
     Serial.println("Internet connected");
-  }
-
-  Serial.println("setup"); 
+  } 
+  
+  Serial.println("setup");    
 }
 
 void loop() {
-  if(receiveflag){
-      receiveflag= false;
-      Serial.println("Data post");
-      post_something();
-  }
+      getDataFromServer();
+      send();
+      lastSendTime = millis();
+      while (millis() - lastSendTime < interval){ 
+        onreceive();
+      }
 }
 
 
-void onReceive(int packetSize)//LoRa receiver interrupt service
+void send()
+{  
+  LoRa.beginPacket();                   // start packet
+  LoRa.write((uint8_t*)&telemtryPacketTX, sizeof(telemtryPacketTX));
+  LoRa.endPacket();     // finish packet and send it
+  LoRa.receive();
+  Serial.print("packet no. sent to slave :");
+  Serial.println(msgCount - 1);
+  Serial.println();
+}
+
+
+void onreceive()//LoRa receiver interrupt service
 {
+   int packetSize = LoRa.parsePacket();
    if (packetSize == 0) return;          // if there's no packet, return
 
-  Serial.print("Received packet '");
-  // read packet header bytes:
-  int recipient = LoRa.read();          // recipient address
-  byte sender = LoRa.read();            // sender address
-  byte incomingLength = LoRa.read();    // incoming msg length
-  LoRa.readBytes((uint8_t *)&telemtryPacket, sizeof(telemtryPacket));
+  LoRa.readBytes((uint8_t *)&telemtryPacketRX, sizeof(telemtryPacketRX));
+    if (telemtryPacketRX.RXnodeID != node) {
+      Serial.println("This message is not for me.");
+      return;                             // skip rest of function
+    }
+
+  Serial.println("Received packet from slave :");
+  Serial.print("nodeID :");
+  Serial.print(telemtryPacketRX.nodeID);
+  Serial.print("  RXnodeID :");
+  Serial.print(telemtryPacketRX.RXnodeID);
+  Serial.print("  msgType :");
+  Serial.print(telemtryPacketRX.msgType);
+  Serial.print("  msgID :");
+  Serial.print(telemtryPacketRX.msgID);
+  Serial.print("  iCommType :");
+  Serial.print(telemtryPacketRX.iCommType);
+  Serial.print("  timeStamp :");
+  Serial.print(telemtryPacketRX.timeStamp);
+  Serial.print("  tempAir :");
+  Serial.print(telemtryPacketRX.tempAir);
+  Serial.print("  latitudeGPS :");
+  Serial.print(((float)telemtryPacketRX.latitudeGPS),6);
+  Serial.print("  longitudeGPS :");
+  Serial.print(((float)telemtryPacketRX.longitudeGPS),6);
+  Serial.print("  ledStatus :");
+  Serial.println(telemtryPacketRX.ledStatus);
+  Serial.println();
+  postDataToServer();
+}
 
 
-  // if the recipient isn't this device or broadcast,
-  if (recipient != localAddress && recipient != 0xFF) {
-    Serial.println("This message is not for me.");
-    return;                             // skip rest of function
+void pktBuild()
+{
+  static int i=0;
+  telemtryPacketTX.nodeID = node;
+  telemtryPacketTX.RXnodeID = slaveNode[i];//random(8,10);
+  telemtryPacketTX.msgType = msgType_CheckStatus;
+  telemtryPacketTX.msgID = msgCount;
+  telemtryPacketTX.iCommType = COMTYPE_LoRa;
+  telemtryPacketTX.timeStamp = millis();
+  telemtryPacketTX.tempAir = 42.78;
+  telemtryPacketTX.latitudeGPS = 19.785667;
+  telemtryPacketTX.longitudeGPS = 20.674597;
+  telemtryPacketTX.ledStatus = 1;
+  msgCount++; 
+  i++;
+  if(i>=(sizeof(slaveNode)/sizeof(slaveNode[0]))){
+    i=i-(sizeof(slaveNode)/sizeof(slaveNode[0]));
   }
 
-  // if message is for this device, or broadcast, print details:
-  Serial.println("Received from: 0x" + String(sender, HEX));
-  Serial.println("Sent to: 0x" + String(recipient, HEX));
-  Serial.println("Message length: " + String(incomingLength)); 
-  Serial.println(telemtryPacket.nodeID);
-  Serial.println(telemtryPacket.msgID);
-  Serial.println(telemtryPacket.iCommType);
-  Serial.println(telemtryPacket.timeStamp);
-  Serial.println(telemtryPacket.tempAir);
-  Serial.println(((float)telemtryPacket.latitudeGPS),6);
-  Serial.println(((float)telemtryPacket.longitudeGPS),6);
-  Serial.println();
-  receiveflag = true;  
 }
 
 
@@ -126,7 +176,6 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
       Serial.println("HTTP_EVENT_HEADER_SENT");
       break;
     case HTTP_EVENT_ON_HEADER:
-      Serial.println();
       Serial.printf("HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
       break;
     case HTTP_EVENT_ON_DATA:
@@ -148,28 +197,19 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
   return ESP_OK;
 }
 
-static esp_err_t post_something()
+static esp_err_t postDataToServer()
 {
-   //String idConvertToStr= String(1001);
-//    String nodeIDConvertToStr= String(telemtryPacket.nodeID);  
-//    String longitudeGPSConvertToStr= String(((float)telemtryPacket.longitudeGPS),6); 
-//    String timeStampConvertToStr = String(telemtryPacket.timeStamp);
-//    String tempAirConvertToStr = String(telemtryPacket.tempAir);
-//    String iCommTypeConvertToStr = String(telemtryPacket.iCommType);
-//    String latitudeGPSConvertToStr = String(((float)telemtryPacket.latitudeGPS),6);
-//    String msgIDStr= String(telemtryPacket.msgID);
-    
-    //String idString ="id="+String(1001);
-    String nodeIDString = "&nodeID="+String(telemtryPacket.nodeID);
-    String GPSString = "&GPS="+String(((float)telemtryPacket.latitudeGPS),6) + "," + String(((float)telemtryPacket.longitudeGPS),6);
-    String timeStampString = "&timeStamp="+String(telemtryPacket.timeStamp);
-    String tempAirString = "&tempAir="+String(telemtryPacket.tempAir);
-    String iCommTypeString = "&iCommType="+String(telemtryPacket.iCommType);
-    String msgIDString = "&msgID="+String(telemtryPacket.msgID);
+    String nodeIDString = "&nodeID="+String(telemtryPacketRX.nodeID);
+    String GPSString = "&GPS="+String(((float)telemtryPacketRX.latitudeGPS),6) + "," + String(((float)telemtryPacketRX.longitudeGPS),6);
+    String timeStampString = "&timeStamp="+String(telemtryPacketRX.timeStamp);
+    String tempAirString = "&tempAir="+String(telemtryPacketRX.tempAir);
+    String iCommTypeString = "&iCommType="+String(telemtryPacketRX.iCommType);
+    String msgIDString = "&msgID="+String(telemtryPacketRX.msgID);
+    String ledStatusString = "&ledStatus="+String(telemtryPacketRX.ledStatus);
     
     String finalStr;
     
-    finalStr = nodeIDString + iCommTypeString + timeStampString + tempAirString + GPSString + msgIDString;
+    finalStr = nodeIDString + iCommTypeString + timeStampString + tempAirString + GPSString + msgIDString + ledStatusString;
     char *post_data;
     
    char *c = strcpy(new char[finalStr.length() + 1], finalStr.c_str());
@@ -191,13 +231,90 @@ static esp_err_t post_something()
   
   esp_http_client_set_post_field(http_client, post_data, strlen(post_data));
   
-  // esp_http_client_set_header(http_client, "Content-Type", "image/jpg"); // sending a jpg file
-
   esp_err_t err = esp_http_client_perform(http_client);
   if (err == ESP_OK) {
     Serial.print("esp_http_client_get_status_code: ");
     Serial.println(esp_http_client_get_status_code(http_client));
+    Serial.println();
   }
 
   esp_http_client_cleanup(http_client);
+}
+
+void getDataFromServer(){
+        static int serverMsgID = 0;
+        static int lastServerMsgID = 0;
+        
+        HTTPClient http;
+        String payload;
+        
+        Serial.print("[HTTP] begin...\n");        // configure traged server and url
+        http.begin(get_url); //HTTP
+
+        Serial.print("[HTTP] GET...\n");
+        int httpCode = http.GET();      // start connection and send HTTP header
+
+        if(httpCode > 0) {        // httpCode will be negative on error
+           Serial.printf("[HTTP] GET... code: %d\n", httpCode);    // HTTP header has been send and Server response header has been handled
+           if(httpCode == HTTP_CODE_OK) {            // file found at server
+              const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(8) + 120;
+              //DynamicJsonDocument doc(capacity);
+              DynamicJsonBuffer jsonBuffer(capacity);
+              JsonObject& root = jsonBuffer.parseObject(http.getString()); 
+
+              unsigned long timeStamp = root["data"][0]["timeStamp"];
+              int serverMsgID = root["data"][0]["msgID"];
+              int nodeID = root["data"][0]["nodeID"];
+              int iCommType =  root["data"][0]["iCommType"];
+              float tempAir = root["data"][0]["tempAir"];
+              bool ledStatus = root["data"][0]["ledStatus"];
+              String latitudeGPS = root["data"][0]["GPS"];
+              
+              if(serverMsgID <= lastServerMsgID){
+                Serial.print("no new msg from server serverMsgID :");
+                Serial.println(serverMsgID);
+                Serial.println();
+                pktBuild();
+                return;
+              }
+              lastServerMsgID = serverMsgID;
+              
+                telemtryPacketTX.nodeID = node;
+                telemtryPacketTX.RXnodeID = root["data"][0]["nodeID"];
+                telemtryPacketTX.msgType = msgType_Command;
+                telemtryPacketTX.msgID = msgCount;
+                telemtryPacketTX.iCommType = root["data"][0]["iCommType"];
+                telemtryPacketTX.timeStamp = millis();
+                telemtryPacketTX.tempAir = root["data"][0]["tempAir"];
+                telemtryPacketTX.latitudeGPS = 19.785667;
+                telemtryPacketTX.longitudeGPS = 20.674597;
+                telemtryPacketTX.ledStatus = root["data"][0]["ledStatus"];
+                msgCount++; 
+                
+
+              Serial.println("Received packet from server :");
+              Serial.print("nodeID :");
+              Serial.print(nodeID);
+              Serial.print("  serverMsgID :");
+              Serial.print(serverMsgID);
+              Serial.print("  iCommType :");
+              Serial.print(iCommType);
+              Serial.print("  timeStamp :");
+              Serial.print(timeStamp);
+              Serial.print("  tempAir :");
+              Serial.print(tempAir);
+              Serial.print("  GPS :");
+              Serial.print(latitudeGPS);
+              Serial.print("  ledStatus :");
+              Serial.println(ledStatus);
+              Serial.println();
+           }
+        } 
+        else {
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+            pktBuild();
+        }
+        
+
+        http.end();
 }
